a8211e57a03e21e8cc87ad859e200d51ee04435c
Stefanie <stefanie.herresthal@uni-bonn.de>
2018-09-15 16:18:28 +0200

    Cleaned

--- GSM_info_search.R
+++ GSM_info_search.R
@@ -4,33 +4,12 @@ library(plyr)
 
 load("E:/Stefanie/Classifier/Paper/Scripts/Datasets.RData")
 
-levels(info.1$GSE)[1:82]
-
-info.1$GSE
-
 # Dataset 1
-res <- data.frame()
-info.1.full <- info.1 %>%
+info.1.char <- info.1 %>%
   mutate_all(as.character)
 
-info.1.full$Filename <- unlist(lapply(strsplit(info.1.full$Filename, split = "\\."), `[[`, 1)) # remove .CEL
+info.1.char$Filename <- unlist(lapply(strsplit(info.1.char$Filename, split = "\\."), `[[`, 1)) # remove .CEL
 
-for(i in 1:length(levels(droplevels(info.1$GSE)))){
-gse <- getGEO(levels(info.1$GSE)[i],GSEMatrix=FALSE)
-
-for(j in 1:length(names(GSMList(gse)))){
-tmp <- as.matrix(lapply(GSMList(gse)[[j]]@header, paste, collapse = ";"))
-tmp <- as.data.frame(t(matrix(unlist(tmp), nrow=length(tmp), byrow=T)))
-colnames(tmp) <- attr(as.matrix(lapply(GSMList(gse)[[j]]@header, paste, collapse = ";")), "dimnames")[[1]]
-res <- rbind.fill(tmp, res)
-}
-res <- res %>%
-  mutate_all(as.character)
-info.1.full <- merge(info.1.full, res, by.x = "Filename", by.y = "geo_accession", all.x = T)
-}
-
-
-#####################################################################
 
 for(i in 1:length(levels(droplevels(info.1$GSE)))){
   gse <- getGEO(levels(info.1$GSE)[i],GSEMatrix=FALSE)
@@ -41,10 +20,13 @@ for(i in 1:length(levels(droplevels(info.1$GSE)))){
     colnames(tmp) <- attr(as.matrix(lapply(GSMList(gse)[[j]]@header, paste, collapse = ";")), "dimnames")[[1]]
     res <- rbind.fill(tmp, res)
   }
+
+res <- merge(info.1.char[info.1.char$GSE %in% levels(droplevels(info.1$GSE))[i],], res, by.x = "Filename", by.y = "geo_accession")  
   
-  write.table(res, paste("E:/Stefanie/Classifier/AML_subtypes/Series_info/", levels(droplevels(info.1$GSE))[i], ".txt", sep = ""), sep = "\t")
+write.table(res, paste("E:/Stefanie/Classifier/AML_subtypes/Series_info/", levels(droplevels(info.1$GSE))[i], ".txt", sep = ""), sep = "\t")
   
 }
+
 
 
 
